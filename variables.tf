0c58e4a42f6c921a703b0a26c44080ff578b50e3
Frank Zhang <44175017+nzhang0430@users.noreply.github.com>
2024-05-23 16:27:25 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "account_id" {
+  description = "The Databricks account ID"
+  type        = string
+}
+
+variable "databricks_instance" {
+  description = "The Databricks instance URL"
+  type        = string
+}
+
+variable "databricks_token" {
+  description = "The Databricks access token"
+  type        = string
+}
